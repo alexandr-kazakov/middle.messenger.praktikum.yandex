06aeef5985313e20b99faf91b461819875c1d25e
Alexandr Kazakov <alexandr.kazakov1@gmail.com>
2022-09-05 15:42:40 +0300

    Working started template

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import { HomePage } from './pages/UserProfile';
+import { Button } from './components/Button';
+
+window.addEventListener('DOMContentLoaded', () => {
+  const root = document.querySelector('#app')!;
+
+  const homePage = new HomePage({ title: 'Home page' });
+
+  root.append(homePage.getContent()!);
+
+  homePage.dispatchComponentDidMount();
+});
---
+++ src/pages/UserProfile/index.ts
@@ -0,0 +1,28 @@
+
+
+import Block from '../../utils/Block';
+import template from './user-profile.pug';
+import { Button } from '../../components/Button';
+
+interface HomePageProps {
+  title: string;
+}
+
+export class HomePage extends Block {
+  constructor(props: HomePageProps) {
+    super('div', props);
+  }
+
+  init() {
+    this.children.button = new Button({
+      label: 'Click me',
+      events: {
+        click: () => console.log('clicked'),
+      },
+    });
+  }
+
+  render() {
+    return this.compile(template, this.props);
+  }
+}
