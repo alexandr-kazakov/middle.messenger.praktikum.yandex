8846daef3bf1e4467a394ec48f4d170e34b44a66
Alexandr Kazakov <alexandr.kazakov1@gmail.com>
2022-09-09 19:27:07 +0300

    fix bug in chat page

--- src/pages/Chat/index.ts
+++ src/pages/Chat/index.ts
@@ -3,7 +3,7 @@
 import Block from '../../utils/Block';
 import template from './chat.pug';
 import { ChatMain } from '../../components/chatMain';
-import { Button } from '../../components/Button';
+import { ChatSidebar } from '../../components/chatSidebar';
 
 interface ChatPageProps {
   events: {
@@ -26,11 +26,6 @@ export class ChatPage extends Block {
     this.children.ChatMain = new ChatMain({
       chatData: { ownerName: 'Вася', lastMessagePreview: "Привет! Предлагаю...", time: "12:49", unreadMessageCounter: "1", ownerPicture: "https://via.placeholder.com/100/16c0b0/ffffff", messages: [{ time: "12:49", data: "Привет! Предлагаю заработок в интернете, специальных знаний не требуется. Просто нажимаешь на кнопку Бабло и получаешь от $4000 в день." }] }
     });
-
-    this.children.ChatMain = new ChatMain({
-      chatData: { ownerName: 'Вася', lastMessagePreview: "Привет! Предлагаю...", time: "12:49", unreadMessageCounter: "1", ownerPicture: "https://via.placeholder.com/100/16c0b0/ffffff", messages: [{ time: "12:49", data: "Привет! Предлагаю заработок в интернете, специальных знаний не требуется. Просто нажимаешь на кнопку Бабло и получаешь от $4000 в день." }] }
-    });
-    // this.children.inputName = new chatMain({});
   }
 
   render() {
